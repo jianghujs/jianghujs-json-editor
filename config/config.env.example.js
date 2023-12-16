61abc4f01ff33390bf6d33c54cea6ef82432730a
xueyuan <xueyuan921@163.com>
2023-12-16 22:45:01 +0800

    add env config, jianghujs 升级

---
+++ config/config.env.example.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = appInfo => {
+
+  return {
+    appType: 'multiApp',
+    appDirectoryLink: '/directory',
+    logger: {
+      outputJSON: true,
+      level: 'INFO',
+      // level: 'DEBUG',
+      // allowDebugAtProd: true,
+      dir: path.join(appInfo.baseDir, 'logs'),
+      contextFormatter(meta) {
+        return `[${meta.date}] [${meta.level}] [${meta.ctx.method} ${meta.ctx.url}] ${meta.message}`;
+      }
+    },
+    knex: {
+      client: {
+        dialect: 'mysql',
+        connection: {
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: 'jianghujs_json_editor'
+        },
+        pool: { min: 0, max: 10 },
+        acquireConnectionTimeout: 30000
+      },
+      app: true
+    }
+  };
+};
